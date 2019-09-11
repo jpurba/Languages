6428d80139f2b79cb2b7a0c51509a2cf4c45fc7e
jpurba <jpurba@ieee.org>
2019-09-11 11:50:14 -0700

    update with average calculation
    update with average calculation

--- python/searchText/searchText.py
+++ python/searchText/searchText.py
@@ -1,17 +1,17 @@
 #import re
 
 fd = open('A01.txt','r')   # open file read only
-buffer = fd.readline()         # read file content
+buffer = fd.readline()     # read file content
 fd.close()                 # close file
 
-if "free" in buffer:   # check string in the buffer
+if "free" in buffer:       # check string in the buffer
     print("free found")
 else:
     print("free is not found")
 
 print(buffer)
 
-fd = open('wssnt10.txt','r')
+fd = open('wssnt10.txt','r') # print another file
 shakes = fd.readline()
 fd.close()
 love = open('love.txt', "w")
@@ -21,7 +21,6 @@ if "Shakespeare" in shakes:
     love.write(shakes)
 else:
     print("Shakespeare is not found")
-
 
 print(shakes)
 
@@ -33,26 +32,52 @@ def wordCount(filename, listwords):
         file.close()
         for word in listwords:
             lower = word.lower()
-            print(lower)
+            #print(lower)
+
+        index = 0
+        index1 = 0
+        index2 = 0
+
         count = 0
+        memTot = 0
+        memTotAvg = 0
+        memUsed = 0
+        memUsedAvg = 0
+        memFree = 0
+        memFreeAvg = 0
         for sentence in read:
+            index += 1
             line = sentence.split()
             #print(line)
             for each in line:
+                index1 += 1
                 line2 = each.lower()
                 line2 = line2.strip("!@#$%^&*()<>?/+:;")
                 #print(line2)
                 if lower == line2:
+                    index2 += 1
                     count += 1
+                    total = int(line[3])  # take total memory
+                    used  = int(line[4])  # take used memory
+                    free  = int(line[5])  # take free memory
+                    #print(total, used, free)
+                    #print("index2 = ", index2, "index1 = ", index1)
+                    memTot = memTot + total
+                    memUsed = memUsed + used
+                    memFree = memFree + free
+                    print(memTot, memUsed, memFree)
 
-
+        #print("index = ", index, "index1 = ", index1, "index2 = ", index2)
         print("Word search :"+lower,", total count: ",count)
+        print(memTot,memUsed, memFree)
+        memTotAvg = memTot / count
+        memFreeAvg = memFree / count
+        memUsedAvg = memUsed / count
+        print(memTotAvg, memUsedAvg, memFreeAvg)
     except FileExistsError:
         print("The file is not there")
 
 
 
-#love.close()
+love.close()
 wordCount("A01.txt", ["Mem"])
-wordCount("wssnt10.txt", ["love"])
-wordCount("test.txt", ["CPU"])
