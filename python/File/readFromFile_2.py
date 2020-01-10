27e7dae210d2701ed56b73ba1f4a71f60f3bcc57
jpurba <jpurba@ieee.org>
2020-01-10 15:20:57 -0800

    update read from file
    update read from file

--- python/File/readFromFile_2.py
+++ python/File/readFromFile_2.py
@@ -1,12 +1,17 @@
 fileName = input ("Enter the file name: ")
 f = open("./python/File/" + fileName, 'r')
 #f = open("./python/File/data.txt","r")
-
+format = []
 lines = f.readlines()
+print(lines[0])
 
 print("Name\tHours\tTotal\n")
 for line in lines:
     line = line.strip()
     print(line)
+    col0 = line.split()
+    print(col0[1])
+    #print(col1)
+    #print(col2)
 
 f.close()
