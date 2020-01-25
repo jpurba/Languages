45ac084d2284d54e7c13fb4e37cca0dad1a288a9
jpurba <jpurba@ieee.org>
2020-01-24 17:49:37 -0800

    Update temperaturConverter.py
    Update temperaturConverter.py

--- python/GUI/temperaturConverter.py
+++ python/GUI/temperaturConverter.py
@@ -1,69 +1,52 @@
 """
-File: taxformwithgui.py
-Project 8.1
-A GUI-based tax calculator.
-
-Computes and prints the total tax, given the income and
-number of dependents (inputs), and a standard deduction of
-$10,000, an exemption amount of $3,000, and a flat tax rate
-of 20%.
+File: temperatureconverter.py
+Project 8.3
+Temperature conversion between Fahrenheit and Celcius.
+Illustrates the use of numeric data fields.
 """
 
 from breezypythongui import EasyFrame
 
 class TemperatureConverter(EasyFrame):
-    """Application window for the tax calculator."""
+    """A termperature conversion program."""
 
     def __init__(self):
         """Sets up the window and the widgets."""
         EasyFrame.__init__(self, title = "Tax Calculator")
-  
         # Label and field for the income
         self.addLabel(text = "Celcius",
                       row = 0, column = 0)
-        self.celciusField = self.addFloatField(value = 0.0,
+        self.celsiusField = self.addFloatField(value = 0.0,
                                               row = 1, column = 0,
                                                precision = 2)
         self.addLabel(text = "Fahrenheit",
                       row = 0, column = 1)
-        self.fahreinheitField = self.addFloatField(value = 32.0,
+        self.fahrField = self.addFloatField(value = 32.0,
                                               row = 1, column = 1,
                                                    precision = 2)
-     
         self.addButton(text = ">>>>", row = 2, column = 0,
                        columnspan = 1, command = self.computeFahr)
 
         self.addButton(text = "<<<<", row = 2, column = 1,
                        columnspan = 1, command = self.computeCelsius)
-        
-
-        """
-        if i_convention.upper() == "C":
-        result = int(round((9 * degree) / 5 + 32))
-        o_convention = "Fahrenheit"
-        elif i_convention.upper() == "F":
-        result = int(round((degree - 32) * 5 / 9))
-        o_convention = "Celsius"
-
-        """
 
     # The controller methods
     def computeFahr(self):
-        """Inputs the Celsius degrees
+        """Inputs the Celcius degrees
         and outputs the Fahrenheit degrees."""
-        celcius = self.celciusField.getNumber()
+        celcius = self.celsiusField.getNumber()
         fahreinheit = ((9 * celcius) / 5 + 32)
-        self.fahreinheitField.setNumber(fahreinheit)
-
+        self.fahrField.setNumber(fahreinheit)
+    
     def computeCelsius(self):
         """Inputs the Fahrenheit degrees
-        and outputs the Celsius degrees."""
-        Fahrenheit = self.fahreinheitField.getNumber()
+        and outputs the Celcius degrees."""
+        Fahrenheit = self.fahrField.getNumber()
         celcius = ((Fahrenheit - 32) * 5 / 9)
-        self.celciusField.setNumber(celcius)
-        
+        self.celsiusField.setNumber(celcius)
         
 def main():
+    """Instantiate and pop up the window."""
     TemperatureConverter().mainloop()
 
 if __name__ == "__main__":
