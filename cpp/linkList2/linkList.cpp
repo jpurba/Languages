bfc531e0d792ccf4a8bae65137ec4aaa863de4bb
Josua Purba <jpurba@ieee.org>
2020-02-12 10:36:09 -0800

    update the linkList.cpp

--- cpp/linkList2/linkList.cpp
+++ cpp/linkList2/linkList.cpp
@@ -3,117 +3,202 @@
 
 using namespace std;
 
+/* Only for the 1st Node */
+void initNode(struct Node *head, int n){
+    head->data = n;
+    head->next = NULL;
+}
 
-void initNode(struct Node *head, int n){}
-void addNode(struct Node *head, int n){}
-void insertFront(struct Node **head, int n){}
-struct Node *searchNode(struct Node *head, int n);
-bool deleteNode(struct Node **head, Node *ptrDel);
-void copyLinkedList(struct Node *node, struct Node **pNew);
-int compareLinkedList(struct Node *node1, struct Node *node2);
-void deleteLinkedList(struct Node **node){}
-void display(struct Node *head){}
+// Appending
+void addNode(struct Node *head, int n){
+    Node *newNode = new Node;
+    newNode->data = n;
+    newNode->next = NULL;
+
+    Node *cur = head;
+    while(cur) {
+         if(cur->next == NULL){
+             cur->next = newNode;
+             return;
+         }
+         cur = cur->next;
+    }
+}
+
+
+
+void insertFront(struct Node **head, int n){
+    Node *newNode = new Node;
+    newNode->data = n;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+struct Node *searchNode(struct Node *head, int n){
+    Node *cur = head;
+    while(cur) {
+        if(cur->data==n) 
+            return cur;
+        cur = cur->next;
+    }
+    cout << "No Node " << n << " in list.\n";
+}
+
+bool deleteNode(struct Node **head, Node *ptrDel){ 
+    Node *cur = *head;
+    if(ptrDel == *head){
+        *head = cur->next;
+        delete ptrDel;
+        return true;
+    }
+    
+    while(cur) {
+        if(cur->next == ptrDel){
+            cur->next = ptrDel->next;
+            delete ptrDel;
+            return true;
+        }
+        cur = cur->next;
+    } 
+    return false;
+}
+
+/* reverse the list */
+struct Node* reverse(struct Node** head){
+    Node *parent = *head;
+    Node *me = parent->next;
+    Node *child = me->next;
+
+    /* make parent as tail */
+    parent->next = NULL;
+    while(child) {
+        me->next = parent;
+        parent = me;
+        me = child;
+        child = child->next;
+    }
+    me->next = parent;
+    *head = me;
+    return *head;
+}
+
+/* Creating a copy of a linked list */
+void copyLinkedList(struct Node *node, struct Node **pNew) {
+    if(node != NULL) {
+        *pNew = new Node;
+        (*pNew)->data = node->data;
+        (*pNew)->next = NULL;
+        copyLinkedList(node->next, &((*pNew)->next));
+    }
+}
+
+/* Compare two linked list */
+int compareLinkedList(struct Node *node1, struct Node *node2){ 
+    static int flag;
+
+    /* Both lists are NULL */
+    if(node1 == NULL && node2 == NULL)
+        flag = 1;
+    else {
+        if(node1 == NULL || node2 == NULL)
+            flag = 0;
+        else if(node1->data != node2->data)
+            flag = 0;
+        else
+            compareLinkedList(node1->next, node2->next);
+    }        
+    return flag;
+}
+
+
+void deleteLinkedList(struct Node **node){
+    struct Node *tmpNode;
+    while(*node) {
+        tmpNode = *node;
+        *node = tmpNode->next;
+        delete tmpNode;
+    }
+}
+
+
+
+void display(struct Node *head){
+    Node *list = head;
+    while(list){
+        cout << list->data << " ";
+        list = list->next;
+    }
+    cout << endl;
+    cout << endl;
+}
+
 
 int main () {
-
-    node1 * n;
-    node1 * t;
-    node1 * x;
-
-    n = new node1;
-    n -> data = 1;
-    t = n;
-    x = n;
-    n -> next = NULL;
-
-
-    cout <<"n data : " << n -> data << endl;
-    cout <<"t data : " << t -> data << endl;
-    cout <<"x data : " << x -> data << endl;
-
-    t -> data = 72;
-    cout <<"Change data on n,t and h to 72 "<< endl;
-    cout <<"n data : " << n -> data << endl;
-    cout <<"t data : " << t -> data << endl;
-    cout <<"x data : " << x -> data << endl;
-
-    n = new node1;
-    n -> data = 2;
-    t = n;
-    x->next = n;
-
-    cout <<"makes n as new node + point t to it "<< endl;
-    cout <<"n data : " << n -> data << endl;
-    cout <<"t data : " << t -> data << endl;
-    cout <<"x data : " << x -> data << endl;
-
-    n = new node1;
-    n -> data = 3;
-    n -> next = NULL; 
-    
-    cout <<"makes n as new node + point t to it "<< endl;
-    cout <<"n data : " << n -> data << endl;
-    cout <<"t data : " << t -> data << endl;
-    cout <<"x data : " << x -> data << endl;
-
-    
-    cout << "\nUse different link list" << endl;
-    node *root;
-    root = new node;
-    root -> num = 4;
-    root -> link = p;
-    p = root;
-
-    node *branch;
-    branch = new node;
-    root -> link = branch;
-    branch -> link = NULL;
-    branch -> num = 6;
-
-    node *branch1;
-    branch1 = new node;
-    branch -> link = branch1;    
-    branch1 -> link = NULL;
-    branch1 -> num=11;
-
-    node *branch2;
-    branch2 = new node;
-    branch1 -> link = branch2;
-    branch2 -> link = NULL;
-    branch2 -> num = 8;
-    
-
-    node *q;
-    for(q=p; q!=NULL; q=q->link)
-    {
-        cout<<q->num;
-        cout<<endl; 
-    }
-
-
-    cout << "\nUse class and print function example" << endl;
-    // use class and print function example
-    Node2* head   = NULL;
-    Node2* second = NULL;
-    Node2* third  = NULL;
-
-    head = new Node2();
-    second = new Node2();
-    third  = new Node2();
-
-    head->data = 1;       // assign data in first node
-    head->next = second;  // Link first node with second
-
-    second->data = 2;
-    second->next = third;
-
-    third->data = 3;
-    third->next = NULL;
-
-    printList(head);
    
-    cout<<"\n"<<endl;
+    struct Node *newHead;
+    struct Node *head = new Node;
+    struct Node *newHead2;
 
+    initNode(head,10);
+    display(head); 
+
+    addNode(head,20);
+    display(head);
+
+    addNode(head,30);
+    display(head);
+
+    addNode(head,35);
+    display(head);
+
+    addNode(head,40);
+    display(head);
+    
+    insertFront(&head,5);
+    display(head);
+
+    int numDel = 5;
+    Node *ptrDelete = searchNode(head,numDel);
+    if(deleteNode(&head,ptrDelete))
+        cout << "Node " << numDel << " deleted!\n";
+    display(head);
+    
+    cout << "The list is copied to newHead2 \n";
+    copyLinkedList(head,&newHead2);
+    display(newHead2);
+
+    cout << "The list is reversed \n";
+    reverse(&head);
+    display(head);
+
+    cout << "The list is copied \n";
+    copyLinkedList(head,&newHead);
+    display(newHead);
+
+    cout << "Comparing the head vs. newHead lists...\n";
+    cout << "Are the two lists same?\n";
+    if(compareLinkedList(head,newHead))
+        cout << "Yes, they are the same !\n";
+    else
+        cout << "No, they are different! \n"; 
+    cout << endl;
+
+
+    cout << "Comparing the head vs. newHead2 lists...\n";
+    cout << "Are the two lists same?\n";
+    if(compareLinkedList(head,newHead2))
+        cout << "Yes, they are the same !\n";
+    else
+        cout << "No, they are different! \n"; 
+    cout << endl;
+
+
+    cout << endl;
+    cout << "Deleting the copied newHead list\n";
+    deleteLinkedList(&newHead);
+    display(newHead);
+
+    cout <<"Main: finished !"<< endl;
     return 0;
 
 }
