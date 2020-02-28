727bdc05c9f67fb1259650306d028edc719bf905
jpurba <jpurba@ieee.org>
2020-02-27 23:23:37 -0800

    add time conversion with test bench file for submission
    add time conversion with test bench file for submission

---
+++ cpp/unit2TimeConversion/unit2timeConversion/apex_code.h
@@ -0,0 +1,22 @@
+#pragma once
+//
+// Don't modify code in this file.
+//
+#include <sstream>
+
+#ifndef APEXLAB_APEX_CODE_H
+#define APEXLAB_APEX_CODE_H
+
+#define SECONDS_IN_MINUTES 60
+#define MINUTES_IN_HOURS   60
+#define MAX_HOURS          12
+#define ZERO                0
+
+int divTwelve(int hours);
+int modSixty(int min);
+int divideSixty(int min);
+
+void minutes_to_12hour_time(std::stringstream& cin, std::stringstream& cout);
+void numbers_to_12hour_time(std::stringstream& cin, std::stringstream& cout);
+
+#endif //APEXLAB_APEX_CODE_H
