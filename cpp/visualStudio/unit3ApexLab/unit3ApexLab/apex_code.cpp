931db541f7fa2f4d9d8133bc93cd794257028f85
jpurba <jpurba@ieee.org>
2020-03-12 00:12:05 -0700

    add string example for input output
    add string example for input output

---
+++ cpp/visualStudio/unit3ApexLab/unit3ApexLab/apex_code.cpp
@@ -0,0 +1,38 @@
+//
+//
+#include <iostream>
+#include "apex_code.h"
+
+using namespace std;
+
+// This function will get the address from input and pass it as output 
+void street_address(stringstream& cin, stringstream& cout)
+{
+
+}
+
+void coffee_sale_item(stringstream& cin, stringstream& cout) {
+    // Place code for outputting the total sales of a coffee item
+    // Pretend this is your main function and you can use cin/cout
+    // as you would in main.
+
+    // If you want to print to the console for debugging use std::cout.
+    // Use cout for your final solution.
+
+    // Get user input
+    //  - drink
+    //  - number sold
+    //  - price
+    // calculate the total sales
+    // output the formatted result
+}
+
+void first_name_and_country(std::stringstream& cin, std::stringstream& cout)
+{
+    // Place code for reading the first name and country here
+    // Pretend this is your main function and you can use cin/cout
+    // as you would in main.
+
+    // If you want to print to the console for debugging use std::cout.
+    // Use cout for your final solution.
+}
