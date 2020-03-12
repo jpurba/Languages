bc482bbe278800b221d87195651bc2e9ff8ea9d0
jpurba <jpurba@ieee.org>
2020-03-12 12:32:05 -0700

    optimize unit3ApexLab
    optimize unit3ApexLab

--- cpp/visualStudio/unit3ApexLab/unit3ApexLab/apex_code.cpp
+++ cpp/visualStudio/unit3ApexLab/unit3ApexLab/apex_code.cpp
@@ -10,13 +10,14 @@ using namespace std;
 // This function will get the address from input and pass it as output 
 void street_address(stringstream& cin, stringstream& cout)
 {
-    string family;
+    const int SKIPCHAR = 18;
     string address;
     string city;
-    getline(cin, family);  // use get line because input string is seperated by "\n"
-    getline(cin, address); // use get line on first part to get the address   
+    //getline(cin, family);  // use get line because input string is seperated by "\n"
+    cin.ignore(SKIPCHAR, '\n');
+    getline(cin, address); // get the address from input
     //getline(cin, city);
-    //cout << "Family: " << family << "; address: " << address << "; city: " << city;
+    //cout << "Address: " << address << "; city: " << city;
     cout << address; // return address as required
 }
 
@@ -41,10 +42,10 @@ void first_name_and_country(std::stringstream& cin, std::stringstream& cout)
 {
     string name, country;
     
-    cin >> name;
-    cin.ignore(29, '.');
-    cin >> country;
-    if (country == ",")
+    cin >> name;               // get the name from input stream
+    cin.ignore(29, '.');       // ignore the address
+    cin >> country;            // get the country 
+    if (country == ",")        // check for extra comma
         cin >> country;
     //cout << "Name: " << name << "; Country: " << country << endl;
     cout << name << " is from " << country;
