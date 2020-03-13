87ccf287c59c34a345f908467f08500169f54a1e
jpurba <jpurba@ieee.org>
2020-03-12 23:13:30 -0700

    update submitUnit3ApexLab with the latest
    update submitUnit3ApexLab with the latest

--- cpp/visualStudio/submitUnit3ApexLab/submitUnit3ApexLab/submitUnit3ApexLab/apex_code.cpp
+++ cpp/visualStudio/submitUnit3ApexLab/submitUnit3ApexLab/submitUnit3ApexLab/apex_code.cpp
@@ -1,6 +1,5 @@
-// submitUnit3ApexLab.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Jeremiah Purba
+// Unit 3 Apex Lab Homework, 3/12/2020
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -9,43 +8,43 @@
 using namespace std;
 
 // This function will get the address from input and pass it as output 
-void street_address(stringstream& cin, stringstream& cout)
-{
+void street_address(stringstream& cin, stringstream& cout) {
+
     const int SKIPCHAR = 18;
     string address;
-    string city;
-   
-    cin.ignore(SKIPCHAR, '\n');  // skip the family
+
+    cin.ignore(SKIPCHAR, '\n');  // ignore the family
     getline(cin, address);       // get the address from input
     cout << address;             // return address as required
 }
 
 // This function will get drink type, number and price from input.
-// It calculates the total sales and return it as result
+// It calculates the total sales and return it as result on output
 void coffee_sale_item(stringstream& cin, stringstream& cout) {
 
     string drink;
     int number;
     double price, total;
 
-    cin >> drink >> number >> price;    // get input data
-    total = number * price;     // calculate total price        
+    cin >> drink >> number >> price;               // get input data
+    total = number * price;                        // calculate total price        
     cout << fixed << showpoint << setprecision(2); // set output precision
-    
-    cout << drink << " total sales = $" << total; //return results
+
+    cout << drink << " total sales = $" << total;  //return results
 }
 
 // This function gets first name and country from input string
 // and returns it on output
-void first_name_and_country(std::stringstream& cin, std::stringstream& cout)
-{
-    string name, country;
+void first_name_and_country(std::stringstream& cin, std::stringstream& cout) {
 
-    cin >> name;               // get the name from input stream
-    cin.ignore(29, '.');       // ignore the address
-    cin >> country;            // get the country 
-    if (country == ",")        // check for extra comma
+    string name, country;
+    const int SKIPCHAR = 29;
+
+    cin >> name;                             // get the name from input stream
+    cin.ignore(SKIPCHAR, '.');               // ignore the address
+    cin >> country;                          // get the country 
+    if (country == ",")                      // check for extra comma
         cin >> country;
-    
+
     cout << name << " is from " << country;  //return name and country
 }
