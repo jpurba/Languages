4dd39b916b9dc37090b7e7879c4b47e115c5d3cd
jpurba <jpurba@ieee.org>
2020-06-19 00:05:34 -0700

    update cpp menu_driven_chap6_7 with display menu with passing array reference
    update cpp menu_driven_chap6_7 with display menu with passing array reference

--- cpp/visualStudio/menu_driven_chap6_7/menu_driven_chap6_7/menu_driven_chap6_7.cpp
+++ cpp/visualStudio/menu_driven_chap6_7/menu_driven_chap6_7/menu_driven_chap6_7.cpp
@@ -1,33 +1,44 @@
-// menu_driven_chap6_7.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* 
+* 
+* Jeremiah Purba 
+* 
+* Programming Assignment REVIEW Chapters 6 & 7
+* 
+* January 18, 2020 
+* 
+* File name: menu_driven_chap6_7.cpp
+*
+*/
 
 #include<iostream>
 using namespace std;
 
-// Assume there is not constraint on the memory
+// Assume there is no constraint on the memory
 // Define constant or read only variables
 const int ROWS = 5;
 const int COLUMNS = 6;
 
-// functions prototype
+// functions prototypes
 void columnSwitch(char str[ROWS][COLUMNS]);
 void totalVowel(char str[ROWS][COLUMNS]);
-void displayMatrix(char str[ROWS][COLUMNS]);
+void display2DArray(char str[ROWS][COLUMNS]);
 void searchCountChar(char str[ROWS][COLUMNS]);
+
 
 int main(){
     
-    const int EXITCHOICES = 5;
+    const int EXITCHOICES = 5;  //Assumed 5 is the exit out
 
     // Note: the number of character in column is 6 
     // extra one character for each row for '\0' null character
-    // Format: str[rows][columns] = { ... }; 
+    // Format: char str[rows][columns] = { ... }; 
     char str[ROWS][COLUMNS] = { "sweet","heart","egrit","clone","odors" };
     int choice = 0;
 
     // This part of the code follow the example from teacher template
-    while (choice != EXITCHOICES) { //Assume 5 is the exit out
+    while (choice != EXITCHOICES) { 
     
+        cout << endl;
         cout << "1. Interchange column 1 and 4" << endl;
         cout << "2. Display the total number of vowels" << endl;
         cout << "3. Display the array in a matrix (rows and columns)" << endl;
@@ -43,11 +54,11 @@ int main(){
             break;
         case 2:   totalVowel(str);       
             break;
-        case 3:   displayMatrix(str);       
+        case 3:   display2DArray(str);       
             break;
         case 4:    searchCountChar(str);       
             break;
-        case 5:    cout << "Thank you for playing this game, goodbye ... ";
+        case 5:    cout << "Thank you for playing this game, goodbye ... " << endl;
             break;
 
         default:
@@ -58,18 +69,28 @@ int main(){
     return 0;
 }
 
-//function for switching column values of column 1 and 4
+// **********************************************************
+// name:      columnSwitch
+// called by: main
+// passed:    char str[ROWS][COLUMNS]
+// returns:   nothing
+// calls:     nobody
+// The columnSwitch function switches column 1 and 4 values *
+// of 2D array of character and it will not display it. It  *
+// prints out message to screen that the value are switched * 
+// **********************************************************
+
 void columnSwitch(char str[ROWS][COLUMNS]){
     
     int i;
-    char temp;               // temporary place for interchange the values 
+    char temp;               // temporary var for interchange the values 
     const int COLUMN_1 = 0;  // first column
     const int COLUMN_4 = 3;  // fourth column
 
     for (i = 0; i < ROWS; i++) {
 
         // interchange the values between column 1 and 4
-        temp = str[i][COLUMN_1];    
+        temp = str[i][COLUMN_1];    // put the value into temporary variable
         str[i][COLUMN_1] = str[i][COLUMN_4];
         str[i][COLUMN_4] = temp;
     }
@@ -77,12 +98,22 @@ void columnSwitch(char str[ROWS][COLUMNS]){
     cout << "The columns in your array have been switched " << endl << endl;
 }
 
-//function for finding and count total number of vowels
+// ********************************************************
+// name:      totalVowel
+// called by: main
+// passed:    char str[ROWS][COLUMNS]
+// returns:   nothing
+// calls:     nobody
+// The totalVowel function finding and count total number *
+// of vowels in the 2D arrays. It prints out message to   *
+// screen the total number of vowel in the 2D array.      * 
+// ********************************************************
+
 void totalVowel(char str[ROWS][COLUMNS]) {
 
     int i;
     int j; 
-    int total = 0;
+    int total = 0;        // for counting the vowel
 
     cout << endl;
 
@@ -94,31 +125,52 @@ void totalVowel(char str[ROWS][COLUMNS]) {
             //check and count vowels here
             if (str[i][j] == 'a' || str[i][j] == 'e' || str[i][j] == 'i' || str[i][j] == 'o' || str[i][j] == 'u') {  
                 
-                total++;
+                total++;  // add total if found the vowel
             }
         }
     }
     cout << "There are " << total << " vowels in this array." << endl << endl;
 }
 
-//function for displaying all values in matrix form
-void displayMatrix(char str[ROWS][COLUMNS]){
-    
-    int i, j;
-    cout << "Here is your array: " << endl << endl;
+// *******************************************************
+// name:      display2DArray
+// called by: main
+// passed:    char str[ROWS][COLUMNS]
+// returns:   nothing
+// calls:     nobody
+// The display2DArray function displays all cell values  *
+// of 2D array in matrix form to screen.                 * 
+// *******************************************************
 
-    // loop through the rows and columns
+void display2DArray(char str[ROWS][COLUMNS]) {
+
+    int i;
+    int j;
+
+    cout << "Here is your array:" << endl << endl;
+
+    // loop through the rows and columns    
     for (i = 0; i < ROWS; i++) {
 
         for (j = 0; j < COLUMNS; j++) {
 
-            cout << str[i][j] << " ";
+            cout << str[i][j] << " "; // print out the value 
         }
         cout << endl << endl;
     }
 }
 
-//function for searching a particular character
+// ***************************************************************
+// name:      searchCountChar
+// called by: main
+// passed:    char str[ROWS][COLUMNS]
+// returns:   nothing
+// calls:     nobody
+// The searchCountChar function search and count the appearance  *
+// of specific character in the 2D arrays. It prints out message *
+// to screen total of instances of character in the 2D array.    * 
+// ***************************************************************
+
 void searchCountChar(char str[ROWS][COLUMNS]){
     
     int i;
@@ -131,7 +183,7 @@ void searchCountChar(char str[ROWS][COLUMNS]){
     cin >> inputChar;
     cout << endl;
 
-    // loop through the rows and columns
+    // loop through the rows and columns 
     for (i = 0; i < ROWS; i++) {
 
         for (j = 0; j < COLUMNS; j++) {
@@ -139,11 +191,11 @@ void searchCountChar(char str[ROWS][COLUMNS]){
             // Check if the character is match
             if (str[i][j] == inputChar) {
 
-                count++;
+                count++;  // add to count if character match
             }
         }
     }
 
     cout << "There are " << count << " instances of your character." << endl << endl;
-
 }
+
