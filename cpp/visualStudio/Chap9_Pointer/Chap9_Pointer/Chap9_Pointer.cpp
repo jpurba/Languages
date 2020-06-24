5c284f9f8646c61763c26ece99fe2385194cd463
jpurba <jpurba@ieee.org>
2020-06-23 17:02:40 -0700

    update chap9_pointer cPP
    update chap9_pointer cPP

--- cpp/visualStudio/Chap9_Pointer/Chap9_Pointer/Chap9_Pointer.cpp
+++ cpp/visualStudio/Chap9_Pointer/Chap9_Pointer/Chap9_Pointer.cpp
@@ -20,24 +20,42 @@ const int ELEMENTINCOLUMN = 5;
 const int TOTALSIZE = 25;
 
 // functions prototypes
-void columnSwitch(char *strp);
-void totalVowel(char* strp);
-void display2DArray(char* strp);
-void searchCountChar(char* strp);
+void menu(void);
+void columnSwitch(char *arrayp);
+void totalVowel(char* arrayp);
+void displayArray(char* arrayp);
+void searchCountChar(char* arrayp);
 
 
 int main() {
 
+    menu();   // call menu function to start the program
+    
+    return 0;
+}
+
+// **********************************************************
+// name:      menu
+// called by: main
+// passed:    nothing
+// returns:   nothing
+// calls:     columnSwitch, totalVowel, displayArray, 
+//            searchCountChar
+// The menu function provides menu and its choices for user  *
+// to perform tasks that are required. It calls the function *
+// accordingly and quit the program when user select 5.      * 
+// **********************************************************
+void menu(void) {
+
     const int EXITCHOICES = 5;  //Assumed 5 is the exit out
 
-    // Note: the number of character in column is 6 
-    // extra one character for each row for '\0' null character
+    // Declaring the 1 dimension array for 25 character / 5 words
     char stringArray[TOTALSIZE] = { 's','w','e','e','t','h','e','a','r','t',
         'e','g','r','i','t','c','l','o','n','e','o','d','o','r','s' };
-    char* strp = stringArray;
+    char* arrayp = stringArray;  // declaring pointer to array
     int choice = 0;
 
-    // This part of the code follow the example from teacher template
+    // This part of the code follow the example from teacher pseudo code
     while (choice != EXITCHOICES) {
 
         cout << endl;
@@ -52,13 +70,13 @@ int main() {
         cout << endl;
         switch (choice) {
 
-        case 1:   columnSwitch(strp);
+        case 1:   columnSwitch(arrayp);
             break;
-        case 2:   totalVowel(strp);
+        case 2:   totalVowel(arrayp);
             break;
-        case 3:   display2DArray(strp);
+        case 3:   displayArray(arrayp);
             break;
-        case 4:    searchCountChar(strp);
+        case 4:    searchCountChar(arrayp);
             break;
         case 5:    cout << "Thank you for playing this game, goodbye ... " << endl;
             break;
@@ -68,35 +86,34 @@ int main() {
             cout << "Please enter a valid number (1-4 or 5 to quit) " << endl << endl;
         }
     }
-
-    delete(strp);
-    return 0;
 }
+
 
 // **********************************************************
 // name:      columnSwitch
 // called by: main
-// passed:    char *strp
+// passed:    char *arrayp
 // returns:   nothing
 // calls:     nobody
 // The columnSwitch function switches column 1 and 4 values *
-// of 2D array of character and it will not display it. It  *
+// of the array of character and it will not display it. It *
 // prints out message to screen that the value are switched * 
 // **********************************************************
 
-void columnSwitch(char* strp) {
+void columnSwitch(char* arrayp) {
 
     int i;
     char temp;               // temporary var for interchange the values 
     const int COLUMN_1 = 0;  // first column
     const int COLUMN_4 = 3;  // fourth column
 
+    // loop through the array
     for (i = 0; i < TOTALSIZE; i++) {
 
         // interchange the values between column 1 and 4
-        temp = *(strp+(i* ELEMENTINCOLUMN)+COLUMN_1);    // put the value into temporary variable
-        *(strp+(i* ELEMENTINCOLUMN)+COLUMN_1) = *(strp+(i* ELEMENTINCOLUMN) + COLUMN_4);
-        *(strp + (i* ELEMENTINCOLUMN) + COLUMN_4) = temp;
+        temp = *(arrayp+(i* ELEMENTINCOLUMN)+COLUMN_1);    // put the value into temporary variable
+        *(arrayp+(i* ELEMENTINCOLUMN)+COLUMN_1) = *(arrayp+(i* ELEMENTINCOLUMN) + COLUMN_4);
+        *(arrayp + (i* ELEMENTINCOLUMN) + COLUMN_4) = temp;
     }
 
     cout << "The columns in your array have been switched " << endl << endl;
@@ -105,26 +122,26 @@ void columnSwitch(char* strp) {
 // ********************************************************
 // name:      totalVowel
 // called by: main
-// passed:    char *strp
+// passed:    char *arrayp
 // returns:   nothing
 // calls:     nobody
 // The totalVowel function finding and count total number *
-// of vowels in the 2D arrays. It prints out message to   *
-// screen the total number of vowel in the 2D array.      * 
+// of vowels in the array. It prints out message to       *
+// screen the total number of vowel in the array.         * 
 // ********************************************************
 
-void totalVowel(char* strp) {
+void totalVowel(char* arrayp) {
 
     int i;
     int total = 0;        // for counting the vowel
 
     cout << endl;
 
-    // loop through the rows and columns
+    // loop through the array
     for (i = 0; i < TOTALSIZE; i++) {
 
         //check and count vowels here
-        if (*(strp+i) == 'a' || *(strp + i) == 'e' || *(strp + i) == 'i' || *(strp + i) == 'o' || *(strp + i) == 'u') {
+        if (*(arrayp+i) == 'a' || *(arrayp + i) == 'e' || *(arrayp + i) == 'i' || *(arrayp + i) == 'o' || *(arrayp + i) == 'u') {
 
             total++;  // add total if found the vowel
         }
@@ -133,43 +150,43 @@ void totalVowel(char* strp) {
 }
 
 // *******************************************************
-// name:      display2DArray
+// name:      displayArray
 // called by: main
 // passed:    char *str
 // returns:   nothing
 // calls:     nobody
-// The display2DArray function displays all cell values  *
-// of 2D array in matrix form to screen.                 * 
+// The displayArray function displays all cell values    *
+// of the array in matrix form to screen.                * 
 // *******************************************************
 
-void display2DArray(char* strp) {
+void displayArray(char* arrayp) {
 
     int i;
     int count=0;
 
     cout << "Here is your array:" << endl << endl;
 
-    // loop through the rows and columns    
+    // loop through the array
     for (i = 0; i < TOTALSIZE; i++) {       
-        cout << * (strp + i) << " "; // print out the value 
-        count++;
-        if(count%5==0)        
-            cout << endl << endl;
+        cout << * (arrayp + i) << " ";      // print out the value 
+        count++;                            // to count character
+        if(count % ELEMENTINCOLUMN == 0)
+            cout << endl << endl;           // seperate each word
     }
 }
 
 // ***************************************************************
 // name:      searchCountChar
 // called by: main
-// passed:    char *strp
+// passed:    char *arrayp
 // returns:   nothing
 // calls:     nobody
 // The searchCountChar function search and count the appearance  *
-// of specific character in the 2D arrays. It prints out message *
-// to screen total of instances of character in the 2D array.    * 
+// of specific character in the array. It prints out message     *
+// to screen total of instances of character in the array.       * 
 // ***************************************************************
 
-void searchCountChar(char* strp) {
+void searchCountChar(char* arrayp) {
 
     int i;
     int count = 0;    // initialize count to zero
@@ -180,11 +197,11 @@ void searchCountChar(char* strp) {
     cin >> inputChar;
     cout << endl;
 
-    // loop through the rows and columns 
+    // loop through the array 
     for (i = 0; i < TOTALSIZE; i++) {
 
-        if (*(strp + i) == inputChar) {
-                count++;   // add to count if character match
+        if (*(arrayp + i) == inputChar) {
+             count++;   // add to count if character match
          }
     }
 
