1d6c77b0db086e2efc5e9b5645d0440adeeba00a
jpurba <jpurba@ieee.org>
2020-08-05 02:56:39 -0700

    add python arithmetic and division example for hackerRank
    add python arithmetic and division example for hackerRank

---
+++ python/math/algorithms/algo1/Division/division.py
@@ -0,0 +1,12 @@
+def main():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+    print(a//b)
+    print(a/b)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/math/algorithms/algo1/arithmetic/arithmetic.py
@@ -0,0 +1,16 @@
+
+def main():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
