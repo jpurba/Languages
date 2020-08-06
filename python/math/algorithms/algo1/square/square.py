905ddab3b98129b1ead9b9b93e7ba2b6fd1c78c2
jpurba <jpurba@ieee.org>
2020-08-06 00:53:46 -0700

    add python square for hacker rank
    add python square for hacker rank

---
+++ python/math/algorithms/algo1/square/square.py
@@ -0,0 +1,11 @@
+
+def main():
+    num = int(input("Enter a number: "))
+    if((num>=0) and (num<=20)):
+        for i in range(num):
+            print(i*i)
+
+
+
+if __name__ == "__main__":
+    main()
