2c552a4b82674a1853b57c02939e65dddecd5bfe
jpurba <jpurba@ieee.org>
2020-09-09 14:37:04 -0700

    add cpp visual studio vectorSort
    add cpp visual studio vectorSort

--- cpp/visualStudio/HackerRank/vectorSort/vectorSort/vectorSort.cpp
+++ cpp/visualStudio/HackerRank/vectorSort/vectorSort/vectorSort.cpp
@@ -2,9 +2,42 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector <int> v;
+    int size;
+    int index;
+    int number;
+
+    cout << "Enter the vector size: ";
+    cin >> size;
+
+    for (index = 0; index < size; index++)
+    {
+        cin >> number;
+        v.push_back(number);
+    }
+    
+    cout << "\nThe content original vector : \n";
+    for (index = 0; index < size; index++)
+    {
+        cout << v[index] << " ";
+    }
+    cout << "\n";
+
+    sort(v.begin(), v.end());
+
+    cout << "The vector content after sort : \n";
+    for (index = 0; index < size; index++)
+    {
+        cout << v[index] << " ";
+    }
+    cout << "\n";
+
 }
 
