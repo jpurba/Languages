3884ee2890c88ce746345ad71ac67b6e7dc0fd4f
jpurba <jpurba@ieee.org>
2020-09-15 01:57:30 -0700

    add java with eclipse mutexSemaphore
    add java with eclipse mutexSemaphore

---
+++ java/eclipse/BLSendReceiveData/BLSendReceiveData/src/com/purba/bl/java/oop/MainActivity.java
@@ -0,0 +1,27 @@
+package com.purba.bl.java.oop;
+
+//import android.Manifest;
+//import android.bluetooth.BluetoothAdapter;
+//import android.bluetooth.BluetoothDevice;
+//import android.content.BroadcastReceiver;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.content.IntentFilter;
+//import android.os.Build;
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ListView;
+
+public class MainActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
