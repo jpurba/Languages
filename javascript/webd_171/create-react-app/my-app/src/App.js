c98462f350c9593a5ebe5873bc21f7b85fb1d56a
jpurba <jpurba@ieee.org>
2020-10-16 21:36:45 -0700

    add javascript for webd 171 create-react-app my-app
    add javascript for webd 171 create-react-app my-app

---
+++ javascript/webd_171/create-react-app/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hi Class !!
+    </div>
+  );
+}
+
+export default App;
